c04edef8e95ca0b828b91c3b72814cbfc8688a91
DanKupchyn <danikk066@gmail.com>
2024-09-02 17:25:03 +0300

    feat: added simple styling for footer

--- src/components/basicLayout/basicLayout.component.tsx
+++ src/components/basicLayout/basicLayout.component.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
 
 import { Header } from '../header/header.component';
+import { Footer } from '../footer/footer.component';
 
 export const BasicLayout = () => (
   <main>
     <Header />
     <Outlet />
-    <footer>Footer</footer>
+    <Footer />
   </main>
 );
---
+++ src/components/footer/footer.component.tsx
@@ -0,0 +1,7 @@
+import styles from './footer.module.scss';
+
+export const Footer = () => (
+  <footer className={styles.footer}>
+    <h6>Just some info</h6>
+  </footer>
+);
